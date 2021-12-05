a6e82135030a7ee66308052528b90b67af7531fa
mateusz <mateusz.hladky@gmail.com>
2021-12-05 12:11:02 +0100

    Added animations time to variables

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const background = document.querySelector('main');
 const lightBtn = document.getElementById('light-mode');
 const darkBtn = document.getElementById('dark-mode');
+const darkAnimationTime = 1; // In seconds
+const lightAnimationTime = 0.75; // In seconds
 
 lightBtn.addEventListener('click', () => {
     background.classList.remove('dark');
-    lightBtn.style.animation = 'rotation 1s ease-out';
-    setTimeout(() => (lightBtn.style.animation = ''), 1000);
+    lightBtn.style.animation = `rotation ${darkAnimationTime}s ease-out`;
+    setTimeout(() => (lightBtn.style.animation = ''), darkAnimationTime * 1000);
 });
 
 darkBtn.addEventListener('click', () => {
     background.classList.add('dark');
-    darkBtn.style.animation = 'rotation 0.75s ease-out';
-    setTimeout(() => (darkBtn.style.animation = ''), 750);
+    darkBtn.style.animation = `rotation ${lightAnimationTime}s ease-out`;
+    setTimeout(() => (darkBtn.style.animation = ''), lightAnimationTime * 1000);
 });
